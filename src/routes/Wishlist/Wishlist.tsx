ff899d67d2b776dfc7bdf91c7befbf7229b04f26
stevejo12 <37164804+stevejo12@users.noreply.github.com>
2023-11-09 07:50:57 +0700

    add: header wishlist page

--- src/routes/Wishlist/Wishlist.tsx
+++ src/routes/Wishlist/Wishlist.tsx
@@ -1,92 +1,34 @@
-import React, { useEffect, useRef, useState } from 'react'
-import FacebookShareIcon from "../../assets/facebook-share.png";
-import TwitterShareIcon from "../../assets/Twitter-share.png";
+import React, { useState } from 'react';
 
 import "./Wishlist.scss";
-import Alert from '../../components/Alert/Alert';
 
-// TO DO
-/**
- * 
- * 
- */
 const Wishlist = () => {
-  const [totalWishes, setTotalWishes] = useState<number>(16);
-  const [wishlistPrivacy, setWishlistPrivacy] = useState<string>("public");
-  const [alertOpen, setAlertOpen] = useState<boolean>(false);
+  const [userHeaderPictureLink, setUserHeaderPictureLink] = useState<string>("");
 
-  const handleAlertClose = (isUserAgree: boolean) => {
-    // if user's agree to changes. no further action needed
-    if (isUserAgree) {
-      setAlertOpen(false);
-      return;
-    }
-
-    // when user disagree, revert back to previous privacy setting
-    setWishlistPrivacy(() => {
-      let val = wishlistPrivacy;
-      if (val === "public") {
-        val = "private";
-      } else {
-        val = "public";
-      }
-
-      return val;
-    });
-    setAlertOpen(false);
-  };
-
-  // console.log("current alert", alertOpen);
-
-  // share link
-  // https://www.tutorialspoint.com/how-to-create-a-share-button-with-different-social-handles-using-html-and-css
   return (
-    <div className="wishlist">
-      <div className="wishlist-left">
-        <div className="wishlist-info">
-          <span className="wishlist-userInfo">My Wishlist{" "}</span>
-          <span className='wishlist-total'>({totalWishes} items)</span>
-        </div>
-        <div className="wishlist-shareLink">
-          <img src={FacebookShareIcon} alt="facebookIcon" />
-          <img src={TwitterShareIcon} alt="twitterIcon" />
+    <div className='wishlist-container'>
+      <div className="wishlist-userHeader">
+        {userHeaderPictureLink && 
+          <img src={userHeaderPictureLink} alt="userHeader" />
+        }
+      </div>
+      <div className="wishlist-content">
+        <div className="wishlist-userProfile">
+          <span className='wishlist-userAvatar'>
+            <img 
+              src="https://images.unsplash.com/photo-1522556189639-b150ed9c4330?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+              alt=""
+              className='wishlist-userAvatar-img'
+            />
+          </span>
+          <div className="wishlist-userNameTag">
+            <span className="wishlist-userName">Jeco Star</span>
+            <span className="wishlist-userTag">@jecostar</span>
+          </div>
         </div>
       </div>
-      <div className="wishlist-right">
-        <div className='wishlist-privacyTitle'>Wishlist Privacy</div>
-        <div className='wishlist-privacyChoice'>
-          <input 
-            type="radio" 
-            name="privacy" 
-            id="public"
-            value="public"
-            checked={wishlistPrivacy === "public"}
-            onClick={() => setAlertOpen(true)}
-            onChange={() => setWishlistPrivacy("public")}
-          />
-          <label htmlFor="public">Public</label>
-          <input 
-            type="radio" 
-            name="privacy" 
-            id="private"
-            value="private"
-            checked={wishlistPrivacy === "private"}
-            onClick={() => setAlertOpen(true)}
-            onChange={() => setWishlistPrivacy("private")}
-          />
-          <label htmlFor="private">Private</label>
-        </div>
-      </div>
-      <Alert
-        open={alertOpen}
-        title="Changing Wishlist Privacy"
-        message='You are about to change your wishlist privacy. Are you sure?'
-        onClose={() => handleAlertClose(false)}
-        onConfirm={() => handleAlertClose(true)}
-        onAbort={() => handleAlertClose(false)}
-      />
     </div>
   )
 }
 
-export default Wishlist;
+export default Wishlist
