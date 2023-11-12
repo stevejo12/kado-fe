5ec84a59f6d3702736b62818be6379563bc7910c
stevejo12 <37164804+stevejo12@users.noreply.github.com>
2023-11-12 20:51:05 +0700

    refactor: wishlist header ||add: wishlist list tab

--- src/routes/Wishlist/Wishlist.tsx
+++ src/routes/Wishlist/Wishlist.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitch, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGift } from '@fortawesome/free-solid-svg-icons';
 
 import "./Wishlist.scss";
 
@@ -14,36 +15,48 @@ const Wishlist = () => {
           <img src={userHeaderPictureLink} alt="userHeader" />
         }
       </div>
-      <div className="wishlist-content">
-        <div className="wishlist-userProfile">
-          <span className='wishlist-userAvatar'>
-            <img 
-              src="https://images.unsplash.com/photo-1522556189639-b150ed9c4330?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-              alt=""
-              className='wishlist-userAvatar-img'
-            />
-          </span>
-          <div className="wishlist-userNameTag">
-            <span className="wishlist-userName">Jeco Star</span>
-            <span className="wishlist-userTag">@jecostar</span>
-          </div>
-          <div className="wishlist-userBioSocials">
-            <p className='wishlist-userBio'>
-              Hello I am a streamer who also models acts and loves sports/fitness. I love all my followers because o am blessed with the nicest and most supportive group ever!
-            </p>
-            <div className="wishlist-userSocials">
-              <a className='wishlist-userSocials-link'>
-                <FontAwesomeIcon icon={faInstagram} />
-              </a>
-              <a className='wishlist-userSocials-link'>
-                <FontAwesomeIcon icon={faTwitter} />
-              </a>
-              <a className='wishlist-userSocials-link'>
-                <FontAwesomeIcon icon={faTwitch}/>
-              </a>
-            </div>
+      <div className="wishlist-userProfile">
+        <span className='wishlist-userAvatar'>
+          <img 
+            src="https://images.unsplash.com/photo-1522556189639-b150ed9c4330?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            alt=""
+            className='wishlist-userAvatar-img'
+          />
+        </span>
+        <div className="wishlist-userNameTag">
+          <span className="wishlist-userName">Jeco Star</span>
+          <span className="wishlist-userTag">@jecostar</span>
+        </div>
+        <div className="wishlist-userBioSocials">
+          <p className='wishlist-userBio'>
+            Hello I am a streamer who also models acts and loves sports/fitness. I love all my followers because o am blessed with the nicest and most supportive group ever!
+          </p>
+          <div className="wishlist-userSocials">
+            <a className='wishlist-userSocials-link' target='_blank'>
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+            <a className='wishlist-userSocials-link' target='_blank'>
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+            <a className='wishlist-userSocials-link' target='_blank'>
+              <FontAwesomeIcon icon={faTwitch}/>
+            </a>
           </div>
         </div>
+      </div>
+      <div className="wishlist-listContainer">
+        <div className="wishlist-listTab">
+          <a className="wishlist-list">
+            <FontAwesomeIcon icon={faGift}/>
+            <p>Wishlist</p>
+          </a>
+          <a className="wishlist-list">
+            <p>Gifters</p>
+            <span>3</span>
+          </a>
+        </div>
+      </div>
+      <div className="wishlist-content">
       </div>
     </div>
   )
